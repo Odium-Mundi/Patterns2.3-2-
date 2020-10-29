471d62ce7fb7beb5a155154fa952af4c78be02eb
Odium-Mundi <61995344+Odium-Mundi@users.noreply.github.com>
2020-10-29 19:36:00 +0300

    Update CardTest.java

--- src/test/java/ru/netology/CardTest.java
+++ src/test/java/ru/netology/CardTest.java
@@ -1,5 +1,9 @@
 package ru.netology;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selectors.byText;
